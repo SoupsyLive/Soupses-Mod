e34622c0ee547e3d0f754531f14a3d5fd4f09b47
SoupsyLive <112738164+SoupsyLive@users.noreply.github.com>
2022-09-02 22:42:27 -0700

    Made a Crystal Item in previous version, fixed it not working Fixed MOD_ID being different from everything else

--- src/main/java/com/soupsy/soupsesmod/SoupsesMod.java
+++ src/main/java/com/soupsy/soupsesmod/SoupsesMod.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SoupsesMod implements ModInitializer {
-	public static final String MOD_ID = "soupsesmod";
+	public static final String MOD_ID = "soupses";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
